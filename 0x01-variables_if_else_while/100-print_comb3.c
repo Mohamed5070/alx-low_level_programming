77c06fd8eb43c08bd9765ce1fcf838762cf2d552
Mohamed5070 <aatik5070example.com>
2023-03-20 16:26:05 -0700

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the numbers from 00 to 99
- * Numbers must be separated by ,followed by a space
- * Numbers should be printed in ascending order with 2 digits
- * You can only use the putchar function
- * Return: 0
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-int n1 = 48;
-int n2 = 48;
-int com = 44;
-while (n1 <= 57)
-{
-n2 = 48;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 57 || n2 != 57)
-{
-putchar(com);
-putchar(32);
-}
-n2 += 1;
-}
-n1 += 1;
-}
-putchar('\n');
-return (0);
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
